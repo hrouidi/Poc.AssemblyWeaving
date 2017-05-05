2908427cb4ce860ab1bd424d00ccc3ce58e114ac
Rouidi Houssam <hrouidi@outloo.com>
2017-05-05 13:35:34 +0200

    Add project files.

---
+++ Fodu.Costura.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Math.Wrapper;
+
+namespace Fody.Costura.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ret = MathWrapper.Add(1, 2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Math.Wrapper/MathWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Math.Wrapper
+{
+    public class MathWrapper
+    {
+        private const string DllName = "Math.Native.dll";
+
+        [DllImport(DllName)]
+        public static extern double Add(double a, double b);
+
+        [DllImport(DllName)]
+        public static extern double Subtract(double a, double b);
+
+        [DllImport(DllName)]
+        public static extern double Multiply(double a, double b);
+
+        [DllImport(DllName)]
+        public static extern double Divide(double a, double b);
+
+    }
+}
